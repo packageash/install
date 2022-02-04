867f62cfd057757921072b0f095a9b18d1d5eb01
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-03 19:56:57 -0600

    Here

--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
 if [[ -f /bin/sudo ]]
 then
-    wget https://github.com/packageash/packer-app-sudo/releases/download/1.0/Packer.deb
+    cd /
+    sudo mkdir .downloaded
+    cd .downloaded
+    sudo wget https://github.com/packageash/packer-app-sudo/releases/download/1.0/Packer.deb
+    sudo dpkg -i Packer.deb
+    cd /
+    sudo rm -r .downloaded
+    echo Installed Packer Sudo Root
+    clear
 else
 if [[ -f /bin/su ]]
 then
-    wget https://github.com/packageash/packer-app-su/releases/download/1.0/Packer.deb
+    cd /
+    su mkdir .downloaded
+    cd .downloaded
+    su wget https://github.com/packageash/packer-app-su/releases/download/1.0/Packer.deb
+    su dpkg -i Packer.deb
+    cd /
+    su rm -r .downloaded
+    echo Installed Packer Su Root
+    clear
 else
-   wget https://github.com/packageash/packer-app-no-root/releases/download/1.0/Packer.deb
+    cd /
+    mkdir .downloaded
+    cd .downloaded
+    wget https://github.com/packageash/packer-app-no-root/releases/download/1.0/Packer.deb
+    dpkg -i Packer.deb
+    cd /
+    rm -r .downloaded
+    clear
 fi
